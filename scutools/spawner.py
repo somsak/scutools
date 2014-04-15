7941531ab15fc47f7ba24156464a593aed62aee3
somsak <somsaks@gmail.com>
2014-04-15 20:45:24 +0700

    Comment unused code

--- scutools/spawner.py
+++ scutools/spawner.py
@@ -42,131 +42,131 @@ class Spawner :
         """
         pass
 
-if has_paramiko :
-    class SshSpawner(Spawner) :
-        def __init__(self, special_arg) :
-            """
-            Initialized generic spawner
-            
-            @param special_arg special argument of SUT
-            """
-            Spawner.__init__(self, special_arg)
-            try :
-                config.max_rshbg = special_arg['max-rshbg']
-                if config.max_rshbg < 1 :
-                    config.max_rshbg = 1
-            except KeyError:
-                pass
-
-            # Mutex for output printing and host_list
-            self.show_out_mutex = threading.Lock()
-            self.host_list_mutex = threading.Lock()
-            self.host_list = []
-
-            # create pool of SshClient equal to number of rshbg
-
-
-        def spawn(self, hostlist, cmd, args, out = None, err = None) :
-            """
-            Spawn the command
-
-            @param hostlist list of host to spawn task
-            @param cmd command name
-            @param args command argument
-            @param out output file object
-            @param err error file object
-            @return 0 if no task fail, otherwise -1
-            """
-            pass
-
-    class SshClient(threading.Thread) :
-        def __init__(self, **kwargs) :
-            threading.Thread.__init__(self)
-            self.spawner = kwargs['spawner'] = spawner
-            self.rt_output = kwargs.get('rt_output', False)
-            self.connect_timeout = kwargs.get('connect_timeout', 15)
-            self.port = kwargs.get('port', 22)
-            self.password = kwargs.get('password', None)
-            self.ssh = paramiko.SSHClient()
-            self.transport = None
-            self.host = ''
-            self.poller = select.poll()
-
-        def show_out(self, outputs) :
-            '''
-            Display output from execution to stdout/stderr
-
-            @param outputs list of output file
-            '''
-            for file,out in outputs :
-                f = open(file, 'r')
-                out.write(f.read())
-                f.close()
-
-        def run(self) :
-            exit = False
-            while True :
-                self.spawner.host_list_mutex.acquire()
-                if self.spawner.host_list :
-                    self.host = self.spawner.host_list[0]
-                    del self.spawner.host_list[0]
-                else :
-                    exit = True
-                self.spawner.host_list_mutex.release()
-                try :
-                    if not exit :
-                        finish = False
-                        if self.transport :
-                            self.transport.close()
-                        self.ssh.connect(host, port=self.port, password=self.password, timeout = self.connect_timeout)
-                        self.transport = self.ssh.get_transport()
-                        session = self.transport.open_session()
-                        session.shutdown_write()
-                        self.poller.register(session, POLLIN)
-                        output_file, output_path = tempfile.mkstemp()
-                        error_file, error_path = tempfile.mkstemp()
-
-                        # main loop to process input/output
-                        while not finish :
-                            events = self.poller.poll()
-                            for event in events :
-                                data = ''
-                                if session.recv_stderr_ready() :
-                                    data = session.recv_stderr(1024)
-                                    storage = output_file
-                                else :
-                                    data = session.recv(1024)
-                                    storage = error_file
-                                if data :
-                                    os.write(storage)
-                                else :
-                                    os.close(output_file)
-                                    os.close(error_file)
-                                    session.shutdown()
-                                    session.close()
-                                    finish = True
-                        # showing output
-                        self.spawner.show_out_mutex.acquire()
-                        self.show_out([(output_path, sys.stdout), (error_path, sys.stderr)])
-                        self.spawner.show_out_mutex.release()
-                        os.remove(output_path)
-                        os.remove(error_path)
-                    else :
-                        break
-                except :
-                    import traceback
-                    traceback.print_exc()
-
-        def __del__(self) :
-            if self.transport :
-                self.transport.close()
-            threading.Thread.__del__(self)
-
-            
-else :
-    class SshSpawner(Spawner) :
-        def __init__(self, special_arg) :
-            raise InvalLauncher('python-paramiko ssh API is not available')
+# if has_paramiko :
+#     class SshSpawner(Spawner) :
+#         def __init__(self, special_arg) :
+#             """
+#             Initialized generic spawner
+#             
+#             @param special_arg special argument of SUT
+#             """
+#             Spawner.__init__(self, special_arg)
+#             try :
+#                 config.max_rshbg = special_arg['max-rshbg']
+#                 if config.max_rshbg < 1 :
+#                     config.max_rshbg = 1
+#             except KeyError:
+#                 pass
+# 
+#             # Mutex for output printing and host_list
+#             self.show_out_mutex = threading.Lock()
+#             self.host_list_mutex = threading.Lock()
+#             self.host_list = []
+# 
+#             # create pool of SshClient equal to number of rshbg
+# 
+# 
+#         def spawn(self, hostlist, cmd, args, out = None, err = None) :
+#             """
+#             Spawn the command
+# 
+#             @param hostlist list of host to spawn task
+#             @param cmd command name
+#             @param args command argument
+#             @param out output file object
+#             @param err error file object
+#             @return 0 if no task fail, otherwise -1
+#             """
+#             pass
+# 
+#     class SshClient(threading.Thread) :
+#         def __init__(self, **kwargs) :
+#             threading.Thread.__init__(self)
+#             self.spawner = kwargs['spawner'] = spawner
+#             self.rt_output = kwargs.get('rt_output', False)
+#             self.connect_timeout = kwargs.get('connect_timeout', 15)
+#             self.port = kwargs.get('port', 22)
+#             self.password = kwargs.get('password', None)
+#             self.ssh = paramiko.SSHClient()
+#             self.transport = None
+#             self.host = ''
+#             self.poller = select.poll()
+# 
+#         def show_out(self, outputs) :
+#             '''
+#             Display output from execution to stdout/stderr
+# 
+#             @param outputs list of output file
+#             '''
+#             for file,out in outputs :
+#                 f = open(file, 'r')
+#                 out.write(f.read())
+#                 f.close()
+# 
+#         def run(self) :
+#             exit = False
+#             while True :
+#                 self.spawner.host_list_mutex.acquire()
+#                 if self.spawner.host_list :
+#                     self.host = self.spawner.host_list[0]
+#                     del self.spawner.host_list[0]
+#                 else :
+#                     exit = True
+#                 self.spawner.host_list_mutex.release()
+#                 try :
+#                     if not exit :
+#                         finish = False
+#                         if self.transport :
+#                             self.transport.close()
+#                         self.ssh.connect(host, port=self.port, password=self.password, timeout = self.connect_timeout)
+#                         self.transport = self.ssh.get_transport()
+#                         session = self.transport.open_session()
+#                         session.shutdown_write()
+#                         self.poller.register(session, POLLIN)
+#                         output_file, output_path = tempfile.mkstemp()
+#                         error_file, error_path = tempfile.mkstemp()
+# 
+#                         # main loop to process input/output
+#                         while not finish :
+#                             events = self.poller.poll()
+#                             for event in events :
+#                                 data = ''
+#                                 if session.recv_stderr_ready() :
+#                                     data = session.recv_stderr(1024)
+#                                     storage = output_file
+#                                 else :
+#                                     data = session.recv(1024)
+#                                     storage = error_file
+#                                 if data :
+#                                     os.write(storage)
+#                                 else :
+#                                     os.close(output_file)
+#                                     os.close(error_file)
+#                                     session.shutdown()
+#                                     session.close()
+#                                     finish = True
+#                         # showing output
+#                         self.spawner.show_out_mutex.acquire()
+#                         self.show_out([(output_path, sys.stdout), (error_path, sys.stderr)])
+#                         self.spawner.show_out_mutex.release()
+#                         os.remove(output_path)
+#                         os.remove(error_path)
+#                     else :
+#                         break
+#                 except :
+#                     import traceback
+#                     traceback.print_exc()
+# 
+#         def __del__(self) :
+#             if self.transport :
+#                 self.transport.close()
+#             threading.Thread.__del__(self)
+# 
+#             
+# else :
+#     class SshSpawner(Spawner) :
+#         def __init__(self, special_arg) :
+#             raise InvalLauncher('python-paramiko ssh API is not available')
 
 class BgfSpawner(Spawner) :
     """
