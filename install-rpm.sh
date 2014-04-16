9ef44efaebb4be37f09c66b81a308df4b95d1ce3
somsak <somsaks@gmail.com>
2014-04-16 15:12:46 +0700

    Fix for old CentOS5

--- install-rpm.sh
+++ install-rpm.sh
@@ -1,3 +1,11 @@
-python setup.py install --root=$RPM_BUILD_ROOT --record=INSTALLED_FILES
+RH_RELEASE=`cat /etc/redhat-release 2> /dev/null | sed 's/.* release \([0-9.]*\) .*/\1/g'`
+case "$RH_RELEASE" in
+    5.*)
+        python2.6 setup.py install --root=$RPM_BUILD_ROOT --record=INSTALLED_FILES
+        ;;
+    *)
+        python setup.py install --root=$RPM_BUILD_ROOT --record=INSTALLED_FILES
+        ;;
+esac
 # 'brp-compress' gzips the man pages without distutils knowing... fix this
 sed -i -e 's@man/man\([[:digit:]]\)/\(.\+\.[[:digit:]]\)$@man/man\1/\2.gz@g' INSTALLED_FILES
