9d06d6b9eb65af9e205441ac160942b57764e44b
somsak <somsaks@gmail.com>
2014-11-17 23:03:58 +0700

    Add pyo for CentOS7

--- install-rpm.sh
+++ install-rpm.sh
@@ -9,3 +9,11 @@ case "$RH_RELEASE" in
 esac
 # 'brp-compress' gzips the man pages without distutils knowing... fix this
 sed -i -e 's@man/man\([[:digit:]]\)/\(.\+\.[[:digit:]]\)$@man/man\1/\2.gz@g' INSTALLED_FILES
+
+case "$RH_RELEASE" in
+    7*)
+        cat INSTALLED_FILES | grep "\.pyc$" | sed 's/\.pyc/.pyo/g' > /tmp/setup_py_scutools.tmp
+        cat /tmp/setup_py_scutools.tmp >> INSTALLED_FILES
+        rm -f /tmp/setup_py_scutools.tmp
+        ;;
+esac
